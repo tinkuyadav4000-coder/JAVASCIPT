865eb2bb4b5d93e3a5e80eb2bf43717017d5e200
Rahul Yadav <tinkuyadav4000@gmail.com>
2026-01-14 12:20:41 +0530

    rahul1

---
+++ third program.js
@@ -0,0 +1,15 @@
+let h=6;
+h+=4;
+console.log('add assign',h);
+let j=5
+j-=6;
+console.log("substraction assign",j);
+let k=9;
+k*=10;
+console.log("multiply assign",k);
+let l=5
+l/=7;
+console.log("division assign",l);
+let m=7;
+m%=8;
+console.log("modulus assign",m);
---
+++ thirdprogram.js
@@ -0,0 +1,33 @@
+//assignment
+let a=4
+let b=11;
+
+console.log(a=b)
+//addition assignment
+
+a+=4;
+console.log(a);
+
+let text="hello";
+text+="world";
+console.log(text);
+
+//substraction assignment
+
+b-=7
+console.log(b);
+
+//multiply assignment
+f=4;
+g=5;
+console.log(f*g);
+
+//divide assignment
+h=2;
+i=6;
+console.log(h/i)
+
+//modulus assignment
+a=8;
+c=9;
+console.log(a%c);
