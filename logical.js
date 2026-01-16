ea97658df6137dec95118ee23ccc01c1bced9a73
Rahul Yadav <tinkuyadav4000@gmail.com>
2026-01-16 14:11:06 +0530

    init: rahul

---
+++ logical.js
@@ -0,0 +1,6 @@
+let a="rahul ";
+ let b="yadav";
+console.log(a+b,"<br>");
+let d="hllo";
+let e="hii";
+console.log(d+e,"<br>");
---
+++ string .js
@@ -0,0 +1,3 @@
+let a="ballia "
+let b="uttarpradesh"
+console.log("ballia=",a+b,"<br>")
